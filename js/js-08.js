6d6786186f16a7c06499444038f894708b5547e1
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-01-20 23:04:55 +0200

    Create js-08.js

---
+++ js/js-08.js
@@ -0,0 +1,22 @@
+// https://youtu.be/9M4XKi25I2M 
+// 44:33 - Engines and Runtime Environments
+
+/*
+JS engines:
+- V8 is chrome's engine
+- SpiderMonkey is FireFox's engine
+
+EcmaScript (ES) – is a standard on which JS is implemented.
+Test 272 is a test that quantifies how good JS implementation is (how well it follows Ecma standard)
+
+Engine is a processor.
+Environment is a location.
+
+Engine is a part of JS environment.
+Environment is usually in a browser.
+But can be in NodeJS, where V8 engine is used.
+
+JS is more like compiled. It is because of how the engines work.
+JIT – just in time compilation.
+
+*/
