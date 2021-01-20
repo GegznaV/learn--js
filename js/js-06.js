8c19270e642dfc335b2f3c2cba0399c8acdf0abd
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-01-20 23:04:47 +0200

    Create js-06.js

---
+++ js/js-06.js
@@ -0,0 +1,25 @@
+// https://youtu.be/9M4XKi25I2M 
+// 31:26 - Variables and Expressions
+
+/* === Variable creation === */
+
+/*
+
+Variable creation consists of 2 steps:
+1) declaration
+2) initialization
+
+*/ 
+
+// both steps as a single line
+var age = 50;
+
+// both steps as separate lines
+var age2;
+age2 = 50;
+
+// In case of numbers, a copy is created:
+var newAge = age;
+
+// Expression
+var message = "newAge = " + newAge; // newAge = 50
