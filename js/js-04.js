a9f172364b6a6d8fc64b210cef4425fcd9890abe
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-01-20 22:59:13 +0200

    Create js-04.js

---
+++ js/js-04.js
@@ -0,0 +1,14 @@
+// https://youtu.be/9M4XKi25I2M 
+// 19:40 - Basic, Input, Output, Comments
+
+
+// single line comment
+
+/*
+multiline
+comment
+*/
+
+var username = prompt(); // prompt window that takes input appears 
+alert(username);         // popup with message appears
+console.log(username);   // writes results to console
