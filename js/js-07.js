355296588ec1de0f7bcaefeec3f635b964a181a9
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-01-20 23:04:51 +0200

    Create js-07.js

---
+++ js/js-07.js
@@ -0,0 +1,19 @@
+// https://youtu.be/9M4XKi25I2M 
+// 38:13 - IIFE, Scope, and Window Object
+
+(function() {
+    // IIFE - immediately invoked function expression
+})();
+
+
+(function() {
+    // IIFE - immediately invoked function expression
+
+    // var makes variable local; e.g., accessible just inside the function
+    var age = 5;
+
+    // without "var" the variable is accessible outside the function scope
+    // i.e., it is global
+    mu = 10;
+    
+})();
