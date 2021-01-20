07f204a44e7e03a061864e0cd2a99b7689a52800
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-01-20 23:06:14 +0200

    Create js-10.js

---
+++ js/js-10.js
@@ -0,0 +1,36 @@
+// https://youtu.be/9M4XKi25I2M
+// 59:21 - Block Scoping
+
+(function () {
+    // IIFE - immediately invoked function expression
+  })();
+  
+  // 'let' creates object in global scope but does not attach to 'window' object.
+  
+  {
+    // block
+  }
+  
+  {
+    // these accessible only in block:
+    let y = 10;
+    const x = 20;
+  
+    console.log(x, y);
+  
+    // This is accessible outside the block
+    var z = 100;
+  }
+  
+  {
+    let y = 10;
+    const x = 20;
+    {
+      let y = 6;
+      console.log(y);
+    }
+  
+    console.log(y);
+  
+  }
+  
